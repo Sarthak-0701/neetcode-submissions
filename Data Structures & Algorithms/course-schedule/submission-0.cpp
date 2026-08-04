522b476618f7f377b7be47c8fdf951123ee90989
Sarthak Shukla <sarthak.shukla0701@gmail.com>
2026-08-04 18:41:04 +0530

    Add: course-schedule - submission-0

---
+++ Data Structures & Algorithms/course-schedule/submission-0.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> adjList(numCourses);
+        vector<int> indegree(numCourses , 0);
+        for(int i = 0 ; i < prerequisites.size() ; i++){
+            int u = prerequisites[i][0];
+            int v = prerequisites[i][1];
+            adjList[v].push_back(u);
+            indegree[u]++;
+        }
+
+        queue<int> q;
+        int count = 0;
+        for(int i = 0 ; i < numCourses ; i++){
+            if(indegree[i] == 0){
+                q.push(i);
+            }
+        }
+        
+        while(!q.empty()){
+            int front = q.front();
+            q.pop();
+            count++;
+            for(int nbr : adjList[front]){
+                indegree[nbr]--;
+
+                if(indegree[nbr] == 0)
+                    q.push(nbr);
+                
+            }
+        }
+        if(count == numCourses)
+            return true;
+
+        return false;
+    }
+
+};
