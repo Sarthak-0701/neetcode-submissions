725ddf91431417b9c940776874ee3f13e66c29ca
Sarthak Shukla <sarthak.shukla0701@gmail.com>
2026-08-27 22:02:17 +0530

    Add: majority-element - submission-0

---
+++ Data Structures & Algorithms/majority-element/submission-0.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int count = 0;
+        int el = 0;
+
+        for(int num : nums){
+            if(count == 0){
+                el = num;
+                count++;
+            }
+            else{
+                if(num == el){
+                    count++;
+                }
+                else{
+                    count--;
+                }
+            }
+        }
+        return el;
+    }
+};
