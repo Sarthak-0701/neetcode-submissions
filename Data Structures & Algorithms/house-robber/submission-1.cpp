dce4496e9297966ca98a09b65f138ddba8c5b90f
Sarthak Shukla <sarthak.shukla0701@gmail.com>
2026-09-07 09:12:27 +0530

    Add: house-robber - submission-1

---
+++ Data Structures & Algorithms/house-robber/submission-1.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+
+    int solve(vector<int>& nums , int n , vector<int>& dp){
+        if(n < 0)    return 0;
+        if(dp[n] != -1) return dp[n];
+        int take = nums[n] + solve(nums , n-2 , dp);
+        int notTake = 0 + solve(nums , n-1 , dp);
+
+        return dp[n] = max(take, notTake);
+    }
+
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dp(n , -1);
+        return solve(nums , n-1 , dp);
+    }
+};
